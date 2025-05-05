47088803fb36c2b63f23f630e03732fde7bda753
Esin Gulbahar <esin.g.gulbahar2@gmail.com>
2025-05-05 15:30:42 +0200

    made pfiles executable

--- user-sixte-init-datalabs.sh
+++ user-sixte-init-datalabs.sh
@@ -7,14 +7,18 @@ echo 'export SIXTE=/opt/sixte/sixte' >> ~/.bashrc
 echo 'export SIMPUT=/opt/sixte/simput' >> ~/.bashrc
 echo '. $SIXTE/bin/sixte-install.sh' >> ~/.bashrc
 echo 'export PFILES="/media/home/pfiles:/opt/sixte/sixte/share/sixte/pfiles:/opt/sixte/simput/share/simput/pfiles:/usr/local/heasoft-6.33.2/x86_64-pc-linux-gnu-libc2.35/syspfiles"' >> ~/.bashrc
+echo 'chmod u+w /media/home/pfiles'>> ~/.bashrc
 
 # Append to .profile
 echo 'export SIXTE=/opt/sixte/sixte' >> ~/.profile
 echo 'export SIMPUT=/opt/sixte/simput' >> ~/.profile
 echo '. $SIXTE/bin/sixte-install.sh' >> ~/.profile
+echo 'export PFILES="/media/home/pfiles:/opt/sixte/sixte/share/sixte/pfiles:/opt/sixte/simput/share/simput/pfiles:/usr/local/heasoft-6.33.2/x86_64-pc-linux-gnu-libc2.35/syspfiles"' >> ~/.profile
+echo 'chmod u+w /media/home/pfiles'>> ~/.profile
 
 # Source it immediately for the current shell
 export SIXTE=/opt/sixte/sixte
 export SIMPUT=/opt/sixte/simput
 . $SIXTE/bin/sixte-install.sh
 export PFILES="/media/home/pfiles:/opt/sixte/sixte/share/sixte/pfiles:/opt/sixte/simput/share/simput/pfiles:/usr/local/heasoft-6.33.2/x86_64-pc-linux-gnu-libc2.35/syspfiles"
+chmod u+w /media/home/pfiles

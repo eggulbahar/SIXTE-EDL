d8a9f5523259cb149bdd7f83112bd85d7a45f429
Esin Gulbahar <esin.g.gulbahar2@gmail.com>
2025-05-06 17:35:51 +0200

    erased unnecessary comments

--- user-sixte-init-datalabs.sh
+++ user-sixte-init-datalabs.sh
@@ -20,29 +20,3 @@ echo "export PFILES=\"$HOME/pfiles;$HEADAS/syspfiles:/opt/sixte/sixte/share/sixt
 echo 'export SIXTE=/opt/sixte/sixte' >> ~/.profile
 echo 'export SIMPUT=/opt/sixte/simput' >> ~/.profile
 echo '. $SIXTE/bin/sixte-install.sh' >> ~/.profile
-#echo 'export locpfiles=$HOME/pfiles' >> ~/.profile
-#echo 'export syspfiles=$HEADAS/syspfiles' >> ~/.profile
-#echo 'export sixtepfiles=$SIXTE/share/sixte/pfiles' >> ~/.profile
-#echo 'export simputpfiles=$SIMPUT/share/simput/pfiles' >> ~/.profile
-#echo 'export PFILES=$locpfiles;$syspfiles:$sixtepfiles:$simputpfiles' >> ~/.profile
-
-#-----------------------------------------------------
-# Source the install immediately for the current shell
-#-----------------------------------------------------
-#export SIXTE=/opt/sixte/sixte
-#export SIMPUT=/opt/sixte/simput
-#. $SIXTE/bin/sixte-install.sh
-
-#-----------------------------------------
-# Handle parameter files for current shell
-#-----------------------------------------
-#locpfiles="$HOME/pfiles"
-#syspfiles="$HEADAS/syspfiles"
-#sixtepfiles="$SIXTE/share/sixte/pfiles"
-#simputpfiles="$SIMPUT/share/simput/pfiles"
-#PFILES="$locpfiles;$syspfiles:$sixtepfiles:$simputpfiles"
-#export PFILES
-
-
-
-
